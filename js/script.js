6f61c562165bf4068837c0c36c18631140c90558
andreas893 <eaa24andrh@students.eaaa.dk>
2025-01-29 16:16:55 +0100

    færdig

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
 "use strict";
 
+let gameRunning = true;
+
+const randomNum = Math.floor(Math.random()* 101);
+
+while (gameRunning){
+    let guess = prompt("Guess a number between 0 and 100");
+
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+        console.log("Please enter a valid number");
+        continue;
+    }
+
+
+    if(guess === randomNum){
+        console.log("Congratulations you guessed right!");
+        alert("You won! The correct number was" + randomNum);
+        gameRunning = false;
+        break;
+    }
+    else if(guess < randomNum){
+        console.log("Try a higher number.");
+    }
+    
+    else{
+        console.log("Try a smaller number");
+    }
+
+    
+}
